2aa9420396bcec4e014d0e20be0d1155b8100b3a
PaulinaKas <49270882+PaulinaKas@users.noreply.github.com>
2025-01-06 22:24:54 +0100

    Code execution order added

---
+++ main.py
@@ -0,0 +1,8 @@
+import words_downloader as downloader
+
+def main():
+    words_df = downloader.download_from_html()
+    words = downloader.convert_to_word_obj(words_df)
+
+if __name__ == "__main__":
+    main()
