ed26eb99c59ec9d841c8d35342e8b43eda9bf108
PaulinaKas <49270882+PaulinaKas@users.noreply.github.com>
2025-01-13 14:33:38 +0100

    Categories updated

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import words_downloader as downloader
 
 def main():
     words_df = downloader.download_from_html()
-    words = downloader.convert_to_word_obj(words_df)
+    # words = downloader.convert_to_word_obj(words_df)
 
 if __name__ == "__main__":
     main()
